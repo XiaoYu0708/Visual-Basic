3e7c49b53c57fb9fab28ec64e873256d783b3e7f
XiaoYu <68182187+XiaoYu0708@users.noreply.github.com>
2023-05-16 10:54:39 +0800

    Add files via upload

---
+++ 1060305/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_05 = New System.Windows.Forms.Button()
+        Me.tb_05 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btn_05
+        '
+        Me.btn_05.Location = New System.Drawing.Point(745, 278)
+        Me.btn_05.Name = "btn_05"
+        Me.btn_05.Size = New System.Drawing.Size(479, 173)
+        Me.btn_05.TabIndex = 0
+        Me.btn_05.Text = "第五題"
+        Me.btn_05.UseVisualStyleBackColor = True
+        '
+        'tb_05
+        '
+        Me.tb_05.Location = New System.Drawing.Point(689, 618)
+        Me.tb_05.Multiline = True
+        Me.tb_05.Name = "tb_05"
+        Me.tb_05.Size = New System.Drawing.Size(618, 296)
+        Me.tb_05.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1960, 1084)
+        Me.Controls.Add(Me.tb_05)
+        Me.Controls.Add(Me.btn_05)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_05 As Button
+    Friend WithEvents tb_05 As TextBox
+End Class
---
+++ 1060305/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Private Sub btn_05_Click(sender As Object, e As EventArgs) Handles btn_05.Click
+        Dim a(2, 2), b(2, 2), c(2, 2)
+        FileOpen(1, "C:\丙設資料\1060305.sm", OpenMode.Input)
+        Input(1, a(1, 1)) : Input(1, a(1, 2))
+        Input(1, a(2, 1)) : Input(1, a(2, 2))
+        Input(1, b(1, 1)) : Input(1, b(1, 2))
+        Input(1, b(2, 1)) : Input(1, b(2, 2))
+        FileClose(1)
+
+        'Dim i = 1
+        'Do While i <= 2
+        '    Dim j = 1
+        '    Do While j <= 2
+        '        c(i, j) = a(i, j) + b(i, j)
+        '        j = j + 1
+        '    Loop
+        '    i = i + 1
+        'Loop
+
+        Dim i = 1
+        Do
+            Dim j = 1
+            Do
+                c(i, j) = a(i, j) + b(i, j)
+                j = j + 1
+            Loop While j <= 2
+            i = i + 1
+        Loop While i <= 2
+
+        'For i = 1 To 2
+        '    For j = 1 To 2
+        '        c(i, j) = a(i, j) + b(i, j)
+        '    Next
+        'Next
+
+        Dim m1
+        m1 = m1 & "[" & c(1, 1) & "     " & c(1, 2) & "]" & vbNewLine
+        m1 = m1 & "[" & c(2, 1) & " " & c(2, 2) & "]" & vbNewLine
+
+        tb_05.Text = "第五題結果：" & vbNewLine & m1
+    End Sub
+End Class
